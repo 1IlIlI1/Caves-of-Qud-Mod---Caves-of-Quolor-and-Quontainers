c0033d65c4fcd7c4603018cbd30b39dd10969057
1IlIlI1 <115104756+1IlIlI1@users.noreply.github.com>
2023-02-03 00:06:23 -0800

    Nightvision scales per level

--- DarkVision.cs
+++ DarkVision.cs
@@ -11,34 +11,28 @@ namespace XRL.World.Parts.Mutation
     {
       this.DisplayName = "Night Vision";
     }
-
     public override bool CanLevel()
     {
       return true;
     }
-
     public override void Register(GameObject Object)
     {
       base.Register(Object);
     }
-
     public override string GetDescription()
     {
       return "You see in the dark.";
     }
-
     public override string GetLevelText(int Level)
     {
       return "You see in the dark, to " + (Level*5) + " distance";;
     }
-
     public override bool WantEvent(int ID, int cascade)
     {
       if (!base.WantEvent(ID, cascade))
         return ID == BeforeRenderEvent.ID;
       return true;
     }
-
     public override bool HandleEvent(BeforeRenderEvent E)
     {
       if (this.ParentObject.IsPlayer())
@@ -49,24 +43,20 @@ namespace XRL.World.Parts.Mutation
       }
       return true;
     }
-
     public override bool FireEvent(Event E)
     {
       return base.FireEvent(E);
     }
-
     public override bool ChangeLevel(int NewLevel)
     {
     this.Radius = 5*NewLevel;
       return base.ChangeLevel(NewLevel);
     }
-
     public override bool Mutate(GameObject GO, int Level)
     {
     this.Radius = 5*Level;
       return base.Mutate(GO, Level);
     }
-
     public override bool Unmutate(GameObject GO)
     {
       return base.Unmutate(GO);
